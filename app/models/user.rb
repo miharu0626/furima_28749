09b691580ffd5d19dbfc7b339051e174c8ca7979
miharu0626 <miharu.pearls@gmail.com>
2020-08-25 20:11:24 +0900

    ユーザー管理機能フロント実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :family_name, presence: true
+  validates :given_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :given_name_kana, presence: true
+  validates :birth_day, presence: true
+
+  has_many :items
+  has_many :purchases
 end

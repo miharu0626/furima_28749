e5ba5c2fd9a7606f92a1ae96c0a7137f34b9c339
miharu0626 <miharu.pearls@gmail.com>
2020-08-29 23:22:33 +0900

    ビュー下書き、ルーティング、コントローラーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 class ItemsController < ApplicationController
   def index
-    
+    @item = Item.all
+  end
+
+  def new
+    @Item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :text)
   end
 end

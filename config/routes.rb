d8d279e0372effdd708cd999a37511e373d8a2e8
miharu0626 <miharu.pearls@gmail.com>
2020-08-30 02:36:44 +0900

    ゴミ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "items#index"
+  root to: "items#index",
   resources :users, only: [:new, :create, :edit, :update]
-            :items 
-  
+            :items
+
 end

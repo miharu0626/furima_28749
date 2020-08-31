ba8503a478bc921c7914fee85f4125716386a265
miharu0626 <miharu.pearls@gmail.com>
2020-08-31 13:26:36 +0900

    エラーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users, only: [:new, :create, :edit, :update]
-            :items
+  resources :items
 
 end

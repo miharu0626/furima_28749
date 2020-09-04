43aff85547b09367bd9806b0695af8f295177917
miharu0626 <miharu.pearls@gmail.com>
2020-09-04 15:24:04 +0900

    FactoryBotの準備2

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   #空の投稿を保存できないようにする
   with_options presence: true do
     validates :name
-    validates :image
+    # validates :image
     validates :detail
     validates :price, format: { with: /\A[0-9]+\z/ }
   end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    price {123456}
+    name { 'あいうえお' }
+    detail { 'あいうえお' }
+    price { 123456 }
+    category_id {Faker::Number.between(from: 1, to: 10) }
+    item_status_id {Faker::Number.between(from: 1, to: 6) }
+    shipping_fee_id {Faker::Number.between(from: 1, to: 2) }
+    shipping_location_id {Faker::Number.between(from: 1, to: 47) }
+    shipping_date_id {Faker::Number.between(from: 1, to: 3) }
+    association :user
   end
 end

317334a55353f957cf2fcfbdadc9746440607255
miharu0626 <miharu.pearls@gmail.com>
2020-09-06 15:56:40 +0900

    デプロイのため不要なファイルを削除

--- db/migrate/20200901034223_rename_shipping_location_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameShippingLocationColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :shipping_location, :shipping_location_id
-  end
-end
--- db/migrate/20200901034359_rename_shipping_date_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameShippingDateColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :shipping_date, :shipping_date_id
-  end
-end

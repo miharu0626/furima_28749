d23f760d165031873f3313b19bdcddd9b3bf70ff
miharu0626 <miharu.pearls@gmail.com>
2020-09-10 18:26:48 +0900

    validation実装

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -4,14 +4,15 @@ class Shipping < ApplicationRecord
 
 # 空で保存できないようにする
 with_options presence: true do
-  validates :post_code
-  validates :prefecture_id
+  validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
   validates :city
   validates :add_num
-  validates :phone_num
+  validates :phone_num, format: {with: /\A\d{10,11}\z/}
   validates :token
 end
 
+  # 各選択が「---」の時は保存できないようにする
+  validates :prefecture_id, numericality: { other_than: 1 }
 
 
   belongs_to :purchase

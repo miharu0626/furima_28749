3aec286a858b94a10c388b818b328ed715f6a321
miharu0626 <miharu.pearls@gmail.com>
2020-09-15 12:22:04 +0900

    PAYJPのキーを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
 
@@ -14,4 +15,14 @@ class ApplicationController < ActionController::Base
                                                        :given_name_kana,
                                                        :birth_day])
   end
+
+  private
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+
 end
